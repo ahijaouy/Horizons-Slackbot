a70cb2bb2ddf21eee37bc90b4d3f1fef66cd4a6f
Oberon <ahijaouy@gatech.edu>
2017-07-17 17:55:26 -0700

    Fix server file

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 
 const app = express();
-const slackRTM = require('./services/slackrtm');
-const routes = require('./routes/routes.js');
+const { rtm, web } = require('./services/slackrtm');
+const routes = require('./routes/routes');
 
-//run slack RTM file
-slackRTM();
 
 const nlp = require('./services/nlp');
-nlp();
 //handle all the routes
 app.use('/', routes);
+
 
 //start the server
 app.listen(3000, function() {
     console.log('Server Listening on port 3000');
 })
+rtm.start();

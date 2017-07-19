14fc266ffb9f023efee034da54183d593a4e8570
Oberon <ahijaouy@gatech.edu>
2017-07-18 20:58:16 -0700

    deploy

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,5 @@ module.exports = {
         'clientID'      : '925901605067-pfe65okm3c1qm5js1imhgiln13ruge4k.apps.googleusercontent.com',
         'clientSecret'  : 'J_7uT9kC7nKQpaXP_2ksQesW',
         'callbackURL'   : 'https://jarvis-horizons.herokuapp.com/connect/callback'
-        //'callbackURL' : 'http://localhost:3000/connect/callback'
     }
 };

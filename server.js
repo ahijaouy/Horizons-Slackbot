1558ad60b2b9ab0ed4443e2b912417622166ebd9
Oberon <ahijaouy@gatech.edu>
2017-07-19 11:09:31 -0700

    updates

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.use('/', routes);
 // Database Connection
 mongoose.connect(dbconfig.url);
 
-
 // Start Server
 app.listen(port, function() {
     console.log('Server Listening on port ' + port);

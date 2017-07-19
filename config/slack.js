63289ac81d5b2a030e144b1a77fabdfdf218840f
Oberon <ahijaouy@gatech.edu>
2017-07-18 18:56:36 -0700

    clean up code and add comments

--- config/slack.js
+++ config/slack.js
@@ -1,6 +1,5 @@
 module.exports = {
-
     'SLACK_BOT_TOKEN' : 'xoxb-215048601639-kLgSGQWQzdxVSjlV9rdp1G2w',
     'WEBHOOK_URL_GENERAL': 'https://hooks.slack.com/services/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',
-    'SLACK_VERIFICATION_TOKEN': 'https://us-central1-horizonshackathon.cloudfunctions.net/helloHttp'
+    'SLACK_VERIFICATION_TOKEN': 'https://jarvis-horizons.herokuapp.com/slack/create_event'
 };

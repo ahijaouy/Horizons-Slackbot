2f27cbfb231f2530ba6b98febcc00a2ad18e5c3f
dchan331 <dchan331@gmail.com>
2017-07-20 11:26:59 -0700

    adding in conflict

--- services/slackService.js
+++ services/slackService.js
@@ -1,6 +1,7 @@
 const { sendQuery } = require('./nlp');
 const auth = require('./authentication');
 const AUTH_PREFIX = 'https://jarvis-horizons.herokuapp.com/';
+const { findFreeTimes } = require('./conflicts');
 
 const { getResponseMessage } = require('./slackUtils');
 const { responseJSON } = require('./slackInteractiveMessages');
@@ -10,7 +11,7 @@ let SLACK_IDS;
 // main message processing method called by slackrtm.js
 // receives a message, checks authorization, returns sendMessage with link if user not authorized
 // or returns promise chain of processing a message
-processMessage = (message, slackIds) => { 
+processMessage = (message, slackIds) => {
     if (slackIds) {
         SLACK_IDS = slackIds;
     }
@@ -18,13 +19,13 @@ processMessage = (message, slackIds) => {
     return new Promise((resolve, reject) => {
         console.log('bp 1: ', message.user);
         auth.checkUser(message.user)
-        .then((authUser) => {            
+        .then((authUser) => {
             console.log('bp 2');
             if (authUser.authenticated) {
                 console.log('authenticated route');
 
                 if (authUser.pending && JSON.parse(authUser.pending).type) {
-                    resolve({pending: true});                    
+                    resolve({pending: true});
                 } else {
                     resolve(getApiResponse(message, authUser));
                 }
@@ -35,15 +36,15 @@ processMessage = (message, slackIds) => {
                 resolve({ send: msg });
             }
         });
-        
+
     });
-} 
+}
 
 // method that takes a message and returns objects with results from AI api
 // return: object with SEND key if rtm.sendMessage is to be used, and the message as its value
 // return: object with POST key if web.chat.postMessage is to be used, and msg + json as value object
 getApiResponse = (message, authUser) => {
-    // console.log('get api response');    
+    // console.log('get api response');
 
     return sendQuery(message.text, authUser._id)
         .then((response) => {
@@ -68,28 +69,43 @@ getApiResponse = (message, authUser) => {
                 const responseMsg = getResponseMessage(data.result.action, data.result.parameters);
                 return { post: { msg: responseMsg, json: responseJSON, data: data.result } };
             } else {
-                // console.log('ACTION IS COMPLETE: MEETING', data.result.parameters);                
-                const responseMsg = getResponseMessage(data.result.action, data.result.parameters);
-                console.log('gar sending slackIds: ', SLACK_IDS)
-               
-               
-                // INSERT TIME CONFLICTS CHECKS
-                
+                console.log('ACTION IS COMPLETE: MEETING', data.result);
 
+                //const responseMsg = getResponseMessage(data.result.action, data.result.parameters);
+                //console.log('gar sending slackIds: ', SLACK_IDS)
+                //return { post: { msg: responseMsg, json: responseJSON, data: data.result, slackIds: SLACK_IDS } };
+                //// added by dom
+                const conflict = true
+                if(!conflict){
+                    const responseMsg = getResponseMessage(data.result.action, data.result.parameters);
+                    console.log('gar sending slackIds: ', SLACK_IDS)
+                    return { post: { msg: responseMsg, json: responseJSON, data: data.result, slackIds: SLACK_IDS } };
 
-                return { post: { msg: responseMsg, json: responseJSON, data: data.result, slackIds: SLACK_IDS } };
-                
+                }else{
+                    const start = new Date(data.result.parameters.date + ' ' + data.result.parameters.time);
+                    /// 777600000 is 9 days in miliseconds
+                    const end = new Date(start.getTime() + 777600000)
+                    console.log('start date', start, 'end date', end);
+                    const duration = data.result.parameters.duration ? data.result.parameters.duration : 30
+                    console.log(duration)
+                    const freeTimes = findFreeTimes(slackIds, start, end, duration)
+                    console.log('free', freeTimes)
+                    return { post: { msg: responseMsg, json: getDropdownJson(freeTimes), data: data.result, slackIds: SLACK_IDS } };
+
+                }
+                /////
             }
         })
         .then((obj) => {
-            // console.log('OBJ: ', obj);
+            console.log('############OBJ: ', obj);
+
             return new Promise(function(resolve, reject) {
                 if (obj.post) {
                     let userPending;
                     if (obj.post.slackIds) {
                         Object.assign({}, obj.post.data.parameters, {slackIds: obj.post.slackIds}, {type: obj.post.data.action} );
                     } else {
-                        Object.assign({}, obj.post.data.parameters, {type: obj.post.data.action} );                        
+                        Object.assign({}, obj.post.data.parameters, {type: obj.post.data.action} );
                     }
                     authUser.pending = JSON.stringify(userPending);
                     authUser.save(() => resolve(obj));

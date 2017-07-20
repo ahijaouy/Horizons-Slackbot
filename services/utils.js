84fd0a2923c59c03c8472935d99542697dc98940
Amanda <ajhwhiz@gmail.com>
2017-07-19 17:38:59 -0700

    remove old code from util

--- services/utils.js
+++ services/utils.js
@@ -39,59 +39,6 @@ const getEndDate = (date, duration = 30) => {
   return new Date(startDate+durationInMs);
 }
 
-//
-// - Param: timeArray -> Array
-//
-// - Description: Finds and recommends event times to avoid conflict
-// //
-//  function findFreeTime(busyArray) {
-//    let freeArray = [];
-//    let busyObject =_.groupBy(busyArray,(dataString)=>{
-//      console.log(new Date(dataString.start),new Date(dataString.start).getDate() )
-//      return new Date(dataString.start).getDate()});
-//    console.log('busyObject', busyObject)
-//    for(var key in busyObject){
-//      const lastItem = busyObject[key][busyObject[key].length-1]
-//      if(!busyObject.hasOwnProperty(key)){
-//        console.log('tried to access a random key');
-//      }else{
-//        busyObject[key]=busyObject[key].sort(
-//          (a,b)=> {
-//          a = new Date(a.start);
-//          b = new Date(b.start);
-//          return a>b ? -1 : a<b ? 1 : 0;
-//        })
-//        if(new Date(busyObject[key][0].start).getHours() > 9){
-//          let end = busyObject[key][0].start;
-//          let temp = new Date(busyObject[key][0].start)
-//          let start = new Date(temp.setHours('9'))
-//          freeArray.push({start,end})
-//        }
-//        for(let slot = 0; slot < busyObject[key].length - 1; slot++){
-//           if((busyArray[slot+1].start-busyArray[slot].end) > 1800000){
-//             freeArray.push({start:busyArray[slot].end, end:busyArray[slot+1].start})
-//           }
-//         }
-//         if(new Date(lastItem.end).getHours() < 17){
-//           let start = lastItem.end;
-//           let temp = new Date(lastItem.end);
-//           let end = new Date(temp.setHours('17'))
-//           freeArray.push({start,end})
-//         }
-//      }
-//    }
-//   //  if(busyArray[0])
-//   //
-//   //  for(let slot = 0; slot < busyArray.length - 1; slot++){
-//   //    if(((busyArray[slot+1].start-busyArray[slot].end) > 1800000)
-//   //    &&(busyArray[slot+1].start.getDate() === busyArray[slot].end.getDate())){
-//   //      freeArray.push({start:busyArray[slot].end, end:busyArray[slot+1].start})
-//   //    }
-//   //  }
-//   return freeArray;
-//  }
-
-
 
 module.exports = {
   linkEmails,

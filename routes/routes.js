12ea92a9502996e7b58c7b24fdd66c6238981da6
Amanda <ajhwhiz@gmail.com>
2017-07-20 19:50:46 -0700

    tracking time in routes

--- routes/routes.js
+++ routes/routes.js
@@ -104,11 +104,13 @@ createGoogleReminder = (res, eventInfo, user) => {
 
 // create Google meeting with attendees, start date, end date, and subject
 createGoogleMeeting = (res, eventInfo, user) => {
-  console.log('REACHES creating meeting method')
   
   let startDate = new Date(eventInfo.date + " " + eventInfo.time);
   // // HARD CODE IN ADDITION OF SEVEN HOURS
   // startDate.setHours(startDate.getHours() + 7);
+
+  console.log('REACHES creating meeting method  : startDate: ', startDate);
+  console.log('could hard code start date to be: ', startDate.getHours() + 7);
   
   const endDate = (eventInfo.duration) ? utils.getEndDate(startDate, eventInfo.duration) : utils.getEndDate(startDate);
   
